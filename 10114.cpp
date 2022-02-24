a51cc5de26902130d069f51eef26ad62fa72ef26
HerrscherOfSentience <91196561+HerrscherOfSentience@users.noreply.github.com>
2022-02-24 15:02:22 +0800

    Update 10114.cpp

--- 10114.cpp
+++ 10114.cpp
@@ -1,59 +1,40 @@
 //UVA 10114 - Loansome Car Buyer
 //やった！
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-double d[101];
+int main() {
+	int duration, depreciation;
+	double downPayment, loan, d[101];
 
-int main(){
-    double duration, downpayment, amount, depreciation;
-    
-    while(cin >> duration >> downpayment >> amount >> depreciation){
-        memset(d, 0, sizeof(d));
-        if(duration < 0) break;
-        double monthly = amount/duration;
-        double carValue = amount + downpayment;
-        double owes = amount;
+	while(true){
+		cin >> duration >> downPayment >> loan >> depreciation;
+		if (duration < 0)
+			break;
 
-        for(int i = 0; i < depreciation; i++){
-            int m;
-            double v; cin >> m >> v;
-            d[m] = v;
-        }
+		int m;
+		double p;
+		for(int i = 0; i < depreciation; i++){
+			cin >> m >> p;
+			for (int i = m; i < 101; i++)
+				d[i] = p;
+		}
 
-        double curDep = d[0];
-        carValue -= (carValue*curDep);
+		int now = 0;
+		double monthly = loan / duration;
+		double curVal = (loan + downPayment) * (1 - d[0]);
+		double curLoan = loan;
+		while (curVal < curLoan) {
+			now++;
+			curLoan -= monthly;
+			curVal = curVal * (1-d[now]);
+		}
 
-        for(int i = 1; i <= duration; i++){
-            if(d[i] != 0) curDep = d[i];
-            carValue -= (carValue*curDep);
-            owes -= monthly;
+		cout << now << " month";
+		if (now > 1) cout << "s";
+		cout << endl;
+	}
 
-            if(owes < carValue){
-                cout << i << " month";
-                if(i > 1) cout << "s";
-                cout << endl;
-                break;
-            }
-        }
-    }
-
-    return 0;
+	return 0;
 }
-
-/*
-30 500.0 15000.0 3
-0 .10
-1 .03
-3 .002
-12 500.0 9999.99 2
-0 .05
-2 .1
-60 2400.0 30000.0 3
-0 .2
-1 .05
-12 .025
--99 0 17000 1
-*/
